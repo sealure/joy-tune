72a4b0a6a8c16d28d6a50fd91128eb6ff527b5eb
SimpleCreator <SimpleCreator@SimpleCreator.com>
2026-07-21 18:17:48 +0800

    build: add media kit audio dependencies for windows and macos
    为Windows和macOS平台添加media_kit音频依赖插件，完善跨平台音频支持配置

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import media_kit_libs_macos_audio
 import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MediaKitLibsMacosAudioPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosAudioPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
